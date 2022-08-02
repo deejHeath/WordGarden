b5ecd0293ab3b4e49e753147e55ee32f552bed83
deejHeath <heathdj@plu.edu>
2022-08-02 16:03:39 -0700

    three buttons and image

--- WordGarden/ViewController.swift
+++ WordGarden/ViewController.swift
@@ -8,12 +8,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func drawLines() {
+        // 1
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 280, height: 250))
 
+        let img = renderer.image { ctx in
+            // 2
+            ctx.cgContext.move(to: CGPoint(x: 20.0, y: 20.0))
+            ctx.cgContext.addLine(to: CGPoint(x: 260.0, y: 230.0))
+            ctx.cgContext.move(to: CGPoint(x: 20.0, y: 230.0))
+            ctx.cgContext.addLine(to: CGPoint(x: 260.0, y: 20.0))
 
+            ctx.cgContext.setLineWidth(10)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+
+            // 3
+            ctx.cgContext.strokePath()
+        }
+
+        imageView.image = img
+    }
+        
+    func drawRectangle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 280, height: 250))
+
+        let img = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 280, height: 250)
+
+            // 4
+            ctx.cgContext.setFillColor(UIColor.green.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.gray.cgColor)
+            ctx.cgContext.setLineWidth(10)
+
+            // 5
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+
+        imageView.image = img
+    }
+        
+    func drawCircle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 280, height: 250))
+
+        let img = renderer.image { ctx in
+            let rect = CGRect(x: 5, y: 5, width: 270, height: 240)
+
+            // 6
+            ctx.cgContext.setFillColor(UIColor.blue.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+
+            ctx.cgContext.addEllipse(in: rect)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+
+        imageView.image = img
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        switch sender.tag {
+                case 0: drawLines()
+                case 1: drawRectangle()
+                case 2: drawCircle()
+                default: print("default")
+            }
+    }
+    
 }
 
